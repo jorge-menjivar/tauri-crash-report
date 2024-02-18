a2ad6d7135fef69f1c7d281671d492e0b659f6ca
Jorge Menjivar <jorgemenjivar1997@gmail.com>
2024-02-18 15:54:55 -0800

    simplify crash example

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
-import { UpdatesProvider } from "./providers/updates";
+import { listen } from "@tauri-apps/api/event";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -13,43 +13,58 @@ function App() {
     setGreetMsg(await invoke("greet", { name }));
   }
 
+  const isMounted = useRef(false);
+
+  const startListeners = useCallback(async () => {
+    if (isMounted.current) {
+      return;
+    }
+    isMounted.current = true;
+
+    await listen("updates", async () => {
+      console.log("It works");
+    });
+  }, []);
+
+  useEffect(() => {
+    startListeners();
+  }, [startListeners]);
+
   return (
-    <UpdatesProvider>
-      <div className="container">
-        <h1>Welcome to Tauri!</h1>
+    <div className="container">
+      <h1>Welcome to Tauri!</h1>
 
-        <div className="row">
-          <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-          </a>
-          <a href="https://tauri.app" target="_blank">
-            <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-
-        <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-        <form
-          className="row"
-          onSubmit={(e) => {
-            e.preventDefault();
-            greet();
-          }}
-        >
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="submit">Greet</button>
-        </form>
-
-        <p>{greetMsg}</p>
+      <div className="row">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </UpdatesProvider>
+
+      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+
+      <p>{greetMsg}</p>
+    </div>
   );
 }
 
